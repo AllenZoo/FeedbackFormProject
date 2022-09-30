7a96ebc974cca44d51ff530af85195d5e691bcfb
AllenZoo <allenzhu2000@hotmail.com>
2022-09-30 12:14:59 -0700

    Update AppFormButtons.jsx
    Cleaned up unused commented code

--- src/pages/App/components/AppFormButtons.jsx
+++ src/pages/App/components/AppFormButtons.jsx
@@ -15,7 +15,6 @@ class AppFormButtons extends Component {
     this.setState({
       selectedOption: e.target.value,
     });
-    // this.props.handleSubmit();
   };
 
   handleSubmit = (e) => {
@@ -24,24 +23,11 @@ class AppFormButtons extends Component {
     this.props.navigate("result", {
       state: { rating: this.state.selectedOption },
     });
-    //this.nav("/result", { state: { rating: this.state.selectedOption } });
-    // window.history.push("/result");
-    // history.pushState(this.state.selectedOption, "/result");
   };
 
   render() {
     return (
       <div className="app-form-buttons-container">
-        {/* <ul>
-          <li className="app-form-button-list">
-            <button className="app-form-button">1</button>
-            <button className="app-form-button">2</button>
-            <button className="app-form-button">3</button>
-            <button className="app-form-button">4</button>
-            <button className="app-form-button-selected">5</button>
-          </li>
-        </ul> */}
-        {/* onSubmit={this.handleSubmit} */}
         <form className="app-form-form" onSubmit={this.handleSubmit}>
           <fieldset
             id="rating"
@@ -107,6 +93,7 @@ class AppFormButtons extends Component {
     );
   }
 }
+
 function WithNavigate(props) {
   let navigate = useNavigate();
   return <AppFormButtons {...props} navigate={navigate} />;
